0a0535f41b9c4fa9ee434b8230a332f92619110a
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-15 19:17:30 +0100

    Update 2015-01-15T18:17:30.357Z

--- app.js
+++ app.js
@@ -25,11 +25,8 @@ if (typeof urlVars.subtitle !== 'undefined') {
 var h1 = document.querySelector('h1');
 var h2 = document.querySelector('h2');
 
-var h1span = document.querySelector('h1 span.inner');
-var h2span = document.querySelector('h2 span.inner');
-
-var h1spanouter = document.querySelector('h1 span.outer');
-var h2spanouter = document.querySelector('h2 span.outer');
+var h1span = document.querySelector('h1 span');
+var h2span = document.querySelector('h2 span');
 
 h1span.innerHTML = coverTitle;
 h2span.innerHTML = coverSubtitle;
@@ -38,8 +35,3 @@ h1.style.width = h1span.offsetWidth + 'px';
 h2.style.width = h2span.offsetWidth + 'px';
 h1.style.height = h1span.offsetHeight + 'px';
 h2.style.height = h2span.offsetHeight + 'px';
-
-h1spanouter.style.width = h1span.offsetWidth + 'px';
-h2spanouter.style.width = h2span.offsetWidth + 'px';
-h1spanouter.style.height = h1span.offsetHeight + 'px';
-h2spanouter.style.height = h2span.offsetHeight + 'px';
