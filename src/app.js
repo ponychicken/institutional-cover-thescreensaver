d2c167315fe86d5be75878621c602fae6b83d5b6
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-15 17:15:00 +0100

    fixes

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ var h2 = document.querySelector('h2');
 var h1span = document.querySelector('h1 span');
 var h2span = document.querySelector('h2 span');
 
-h1span.innerText = coverTitle;
-h2span.innerText = coverSubtitle;
+h1span.innerHTML = coverTitle;
+h2span.innerHTML = coverSubtitle;
 
 h1.style.width = h1span.offsetWidth + 'px';
 h2.style.width = h2span.offsetWidth + 'px';
