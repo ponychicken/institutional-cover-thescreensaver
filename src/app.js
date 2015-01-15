1b22e71fe00a4eb2bec42c8b88f4aade9f3ad361
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-15 11:07:24 +0100

    nice, now start at middle

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
-document.querySelector('h1').innerText = coverTitle;
-document.querySelector('h2').innerText = coverSubtitle;
+var h1 = document.querySelector('h1');
+var h2 = document.querySelector('h2');
+
+var h1span = document.querySelector('h1 span');
+var h2span = document.querySelector('h2 span');
+
+h1span.innerText = coverTitle;
+h2span.innerText = coverSubtitle;
+
+h1.style.width = h1span.offsetWidth + 'px';
+h2.style.width = h2span.offsetWidth + 'px';
+h1.style.height = h1span.offsetHeight + 'px';
+h2.style.height = h2span.offsetHeight + 'px';
