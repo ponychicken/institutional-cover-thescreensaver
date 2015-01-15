ce89c804e81223b8ec5ef1b641a2df7443ca0065
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-01-15 19:14:00 +0100

    Working fine, but still fucked in IE

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 var h1 = document.querySelector('h1');
 var h2 = document.querySelector('h2');
 
-var h1span = document.querySelector('h1 span span');
-var h2span = document.querySelector('h2 span span');
+var h1span = document.querySelector('h1 span.inner');
+var h2span = document.querySelector('h2 span.inner');
+
+var h1spanouter = document.querySelector('h1 span.outer');
+var h2spanouter = document.querySelector('h2 span.outer');
 
 h1span.innerHTML = coverTitle;
 h2span.innerHTML = coverSubtitle;
@@ -11,3 +14,8 @@ h1.style.width = h1span.offsetWidth + 'px';
 h2.style.width = h2span.offsetWidth + 'px';
 h1.style.height = h1span.offsetHeight + 'px';
 h2.style.height = h2span.offsetHeight + 'px';
+
+h1spanouter.style.width = h1span.offsetWidth + 'px';
+h2spanouter.style.width = h2span.offsetWidth + 'px';
+h1spanouter.style.height = h1span.offsetHeight + 'px';
+h2spanouter.style.height = h2span.offsetHeight + 'px';
