ccf3a34eb200b05931a59c74c49cedb33edbe80a
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-03-20 14:49:49 +0100

    window.postmessage

--- src/app.js
+++ src/app.js
@@ -5,24 +5,34 @@ if (window.navigator.userAgent.indexOf('Trident') != -1 || window.navigator.user
 }
 
 
-var h1 = document.querySelector('h1');
-var h2 = document.querySelector('h2');
+getTitleData(function (msg) {
+  var h1 = document.querySelector('h1');
+  var h2 = document.querySelector('h2');
 
-var h1span = document.querySelector('h1 span.inner');
-var h2span = document.querySelector('h2 span.inner');
+  var h1span = document.querySelector('h1 span.inner');
+  var h2span = document.querySelector('h2 span.inner');
 
-var h1spanouter = document.querySelector('h1 span.outer');
-var h2spanouter = document.querySelector('h2 span.outer');
+  var h1spanouter = document.querySelector('h1 span.outer');
+  var h2spanouter = document.querySelector('h2 span.outer');
 
-h1span.innerHTML = coverTitle;
-h2span.innerHTML = coverSubtitle;
+  document.querySelector('h1').innerHTML = msg.title;
+  document.querySelector('h2').innerHTML = msg.subtitle;
+  
+  h1.style.width = h1span.offsetWidth + 'px';
+  h2.style.width = h2span.offsetWidth + 'px';
+  h1.style.height = h1span.offsetHeight + 'px';
+  h2.style.height = h2span.offsetHeight + 'px';
 
-h1.style.width = h1span.offsetWidth + 'px';
-h2.style.width = h2span.offsetWidth + 'px';
-h1.style.height = h1span.offsetHeight + 'px';
-h2.style.height = h2span.offsetHeight + 'px';
-
-h1spanouter.style.width = h1span.offsetWidth + 'px';
-h2spanouter.style.width = h2span.offsetWidth + 'px';
-h1spanouter.style.height = h1span.offsetHeight + 'px';
-h2spanouter.style.height = h2span.offsetHeight + 'px';
+  h1spanouter.style.width = h1span.offsetWidth + 'px';
+  h2spanouter.style.width = h2span.offsetWidth + 'px';
+  h1spanouter.style.height = h1span.offsetHeight + 'px';
+  h2spanouter.style.height = h2span.offsetHeight + 'px';
+  
+  // Add link to body to have the parent redirect to the magazine URL
+  if (msg.isHomepage) {
+    document.body.addEventListener('click', function() {
+      redirectParent(msg.domain_path);
+    });
+    document.body.style.cursor = 'pointer';
+  }
+});
